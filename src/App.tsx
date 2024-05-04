102af9ca1c429bda3768ac166aab74cb5e124d25
Hoang Long <longpaus@gmail.com>
2024-05-04 20:47:39 +1000

    Add sorting icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Home from "./pages/Home";
+import TaskController from "./components/TaskController";
 
 function App() {
   return (
     <div className="bg-background">
-      <Home/>
+      <TaskController/>
 
     </div>
   );
