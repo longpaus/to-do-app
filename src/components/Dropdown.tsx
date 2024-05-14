de7c3e4b18088301ab2c9b6ebcfb6c8f415bdc31
Hoang Long <longpaus@gmail.com>
2024-05-15 08:51:49 +1000

    refactor the dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,23 +1,41 @@
-import React, { ReactNode } from "react";
+import React, {ReactNode, useEffect, useRef} from "react";
 
 interface DropDownProps {
   children: ReactNode;
   title: ReactNode;
   open: boolean;
   setOpenFunc: (open: boolean) => void;
+  detectOutsideClick?: boolean;
 }
 
 export default function Dropdown(props: DropDownProps) {
+  const dropDownRef = useRef<HTMLDivElement>(null);
   const handleButtonClick = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
     props.setOpenFunc(!props.open);
   };
+
+  useEffect(() => {
+    const handleOutsideClick = (e: any) => {
+      if (dropDownRef.current && !dropDownRef.current.contains(e.target)) {
+        props.setOpenFunc(false);
+      }
+    };
+
+    if (props.detectOutsideClick) {
+      document.addEventListener("click", handleOutsideClick);
+    }
+    return () => {
+      document.removeEventListener("click", handleOutsideClick);
+    };
+  }, []);
   return (
     <div className="relative">
       <div onClick={handleButtonClick}>{props.title}</div>
       {props.open && (
         <div
           className={`absolute z-10 rounded-md transition-opacity duration-1000`}
+          ref={dropDownRef}
         >
           {props.children}
         </div>
