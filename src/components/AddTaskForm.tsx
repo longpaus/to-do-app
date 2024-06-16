bc3a8d24ab03f604eeb2973585a91383d056c608
Hoang Long <longpaus@gmail.com>
2024-06-17 09:56:17 +1000

    use a calendar icon instead of "none"

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -3,54 +3,56 @@ import Dropdown from "./menus/Dropdown";
 import {defaultStates, useStore} from "../store";
 import Calendar from './calendar/Calendar';
 import {formatDate} from "../utils/date.js";
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 interface AddTaskFormProps {
-  onSubmit: (e: FormEvent<HTMLFormElement>) => void;
-  task: string;
-  onChangeTaskName: (e: ChangeEvent<HTMLInputElement>) => void;
+    onSubmit: (e: FormEvent<HTMLFormElement>) => void;
+    task: string;
+    onChangeTaskName: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 export default function AddTaskForm({onSubmit, task, onChangeTaskName}: Readonly<AddTaskFormProps>) {
-  const [calenderOpen, setCalenderOpen] = React.useState(false);
-  const store = useStore();
+    const [calenderOpen, setCalenderOpen] = React.useState(false);
+    const store = useStore();
 
-  return (
-    <form onSubmit={onSubmit}>
-      <div className="relative">
-        <input type="text" id="newTask"
-               className="block w-full p-4 ps-5 text-sm dark:text-darkOnSurface text-lightOnSurface border rounded-lg bg-lightSurface dark:bg-darkSurface focus:ring-blue-500 focus:border-blue-500"
-               placeholder="Add New Task"
-               required
-               value={task}
-               onChange={onChangeTaskName}
-        />
-        <div className="absolute bottom-3 end-20  ">
-          <Dropdown
-            title={
-              <div className="text-blue-500 p-1 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md">
-                {formatDate(store.globalDueDate)}
-              </div>
-            }
-            open={calenderOpen}
-            setOpenFunc={open => setCalenderOpen(open)}
-            detectOutsideClick
-          >
-            <div className="absolute mt-4">
-              <Calendar
-                onResetHandler={() => store.upDateGlobalDueDate(defaultStates.globalDueDate)}
-                onOkayHandler={(dueDate) => store.upDateGlobalDueDate(dueDate)}
-                dueDate={store.globalDueDate}
-              />
+    return (
+        <form onSubmit={onSubmit}>
+            <div className="relative">
+                <input type="text" id="newTask"
+                       className="block w-full p-4 ps-5 text-sm dark:text-darkOnSurface text-lightOnSurface border rounded-lg bg-lightSurface dark:bg-darkSurface focus:ring-blue-500 focus:border-blue-500"
+                       placeholder="Add New Task"
+                       required
+                       value={task}
+                       onChange={onChangeTaskName}
+                />
+                <div className="absolute bottom-3 end-20  ">
+                    <Dropdown
+                        title={
+                            <div
+                                className="text-blue-500 p-1 cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 rounded-md">
+                                {store.globalDueDate ? formatDate(store.globalDueDate) : <CalendarMonthIcon/>}
+                            </div>
+                        }
+                        open={calenderOpen}
+                        setOpenFunc={open => setCalenderOpen(open)}
+                        detectOutsideClick
+                    >
+                        <div className="absolute mt-4">
+                            <Calendar
+                                resetDueDateHandler={() => store.upDateGlobalDueDate(defaultStates.globalDueDate)}
+                                changeDueDateHandler={(dueDate) => store.upDateGlobalDueDate(dueDate)}
+                                dueDate={store.globalDueDate}
+                            />
+                        </div>
+                    </Dropdown>
+                </div>
+
+                <button type="submit"
+                        className="absolute end-3 bottom-2.5 text-onPrimary bg-primary h-9 hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-purple-500 focus:border-purple-500 font-medium rounded-lg px-4 py-2 text-sm"
+                >
+                    Add
+                </button>
             </div>
-          </Dropdown>
-        </div>
-
-        <button type="submit"
-                className="absolute end-3 bottom-2.5 text-onPrimary bg-primary h-9 hover:bg-purple-500 focus:ring-4 focus:outline-none focus:ring-purple-500 focus:border-purple-500 font-medium rounded-lg px-4 py-2 text-sm"
-        >
-          Add
-        </button>
-      </div>
-    </form>
-  )
+        </form>
+    )
 }
--- src/components/TaskController.tsx
+++ src/components/TaskController.tsx
@@ -10,126 +10,126 @@ import {sortTasks} from "../utils/SortingTasks";
 import {TasksGroups} from "../types/GroupStrategyTypes";
 
 export default function TaskController() {
-  const store = useStore();
-  const [task, setTask] = useState<string>("");
-  const [tasksGroups, setTasksGroups] = useState<TasksGroups>(() => groupTasks(store.groupTask, []));
+    const store = useStore();
+    const [task, setTask] = useState<string>("");
+    const [tasksGroups, setTasksGroups] = useState<TasksGroups>(() => groupTasks(store.groupTask, []));
 
-  useEffect(() => {
-    setTasksGroups(() => groupTasks(store.groupTask, mergeGroupsIntoArray(tasksGroups)));
-  }, [store.groupTask]);
+    useEffect(() => {
+        setTasksGroups(() => groupTasks(store.groupTask, mergeGroupsIntoArray(tasksGroups)));
+    }, [store.groupTask]);
 
-  const updateTaskHandler = (group: string) => (taskId: TaskId) => (updatedTask: Task, changeTaskGroup: boolean = false) => {
-    updateTask(group, taskId, updatedTask);
-    if (changeTaskGroup) {
-      removeTaskFromList(taskId, group);
-      addTaskToList(updatedTask, getGroupKey(updatedTask, store.groupTask));
-    }
-  }
-
-  useEffect(() => {
-    Object.keys(tasksGroups).forEach(group => {
-      setTasksGroups((prevState: TasksGroups) => ({
-          ...prevState,
-          [group]: sortTasks(store.sortTask, prevState[group])
+    const updateTaskHandler = (group: string) => (taskId: TaskId) => (updatedTask: Task, changeTaskGroup: boolean = false) => {
+        updateTask(group, taskId, updatedTask);
+        if (changeTaskGroup) {
+            removeTaskFromList(taskId, group);
+            addTaskToList(updatedTask, getGroupKey(updatedTask, store.groupTask));
         }
-      ));
-    })
-  }, [store.sortTask]);
+    }
 
-  const addTask = (newTask: Task): void => {
-    addTaskToList(newTask, getGroupKey(newTask, store.groupTask))
-  }
-  const addTaskToList = (task: Task, groupName: string) => {
-    switch (store.sortTask) {
-      case "Date":
-        setTasksGroups((prevState: TasksGroups) => ({
-          ...prevState,
-          [groupName]: [task, ...prevState[groupName]]
-        }))
-        break;
-      case "Title":
+    useEffect(() => {
+        Object.keys(tasksGroups).forEach(group => {
+            setTasksGroups((prevState: TasksGroups) => ({
+                    ...prevState,
+                    [group]: sortTasks(store.sortTask, prevState[group])
+                }
+            ));
+        })
+    }, [store.sortTask]);
+
+    const addTask = (newTask: Task): void => {
+        addTaskToList(newTask, getGroupKey(newTask, store.groupTask))
+    }
+    const addTaskToList = (task: Task, groupName: string) => {
+        switch (store.sortTask) {
+            case "Date":
+                setTasksGroups((prevState: TasksGroups) => ({
+                    ...prevState,
+                    [groupName]: [task, ...prevState[groupName]]
+                }))
+                break;
+            case "Title":
+                setTasksGroups((prevState: TasksGroups) => {
+                    const newList = [...prevState[groupName]];
+                    let index = 0;
+                    while (index < newList.length && task.name.localeCompare(newList[index].name) > 0) {
+                        index++;
+                    }
+                    newList.splice(index, 0, task);
+                    return {
+                        ...prevState,
+                        [groupName]: newList
+                    };
+                });
+        }
+
+    };
+
+    const removeTaskFromList = (id: TaskId, groupName: string) => {
         setTasksGroups((prevState: TasksGroups) => {
-          const newList = [...prevState[groupName]];
-          let index = 0;
-          while (index < newList.length && task.name.localeCompare(newList[index].name) > 0) {
-            index++;
-          }
-          newList.splice(index, 0, task);
-          return {
-            ...prevState,
-            [groupName]: newList
-          };
+            const newList = [...prevState[groupName]].filter((task) => task.id !== id);
+            return {
+                ...prevState,
+                [groupName]: newList,
+            };
         });
-    }
+    };
 
-  };
+    const updateTask = (groupName: string, taskId: TaskId, updatedTask: Partial<Task>) => {
+        setTasksGroups(prevState => {
+            const newTasks = prevState[groupName].map((task: Task) => {
+                if (task.id === taskId) {
+                    return {...task, ...updatedTask};
+                }
+                return task;
+            });
+            return {
+                ...prevState,
+                [groupName]: newTasks
+            }
+        })
 
-  const removeTaskFromList = (id: TaskId, groupName: string) => {
-    setTasksGroups((prevState: TasksGroups) => {
-      const newList = [...prevState[groupName]].filter((task) => task.id !== id);
-      return {
-        ...prevState,
-        [groupName]: newList,
-      };
-    });
-  };
+    };
 
-  const updateTask = (groupName: string, taskId: TaskId, updatedTask: Partial<Task>) => {
-    setTasksGroups(prevState => {
-      const newTasks = prevState[groupName].map((task: Task) => {
-        if (task.id === taskId) {
-          return {...task, ...updatedTask};
+    const submitTaskHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (task.length !== 0) {
+            const newTask: Task = {
+                name: task,
+                id: uuid(),
+                creationTime: new Date(),
+                dueDate: store.globalDueDate,
+                completed: false,
+            };
+            addTask(newTask);
+            setTask("");
         }
-        return task;
-      });
-      return {
-        ...prevState,
-        [groupName]: newTasks
-      }
-    })
+    };
 
-  };
-
-  const submitTaskHandler = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (task.length !== 0) {
-      const newTask: Task = {
-        name: task,
-        id: uuid(),
-        creationTime: new Date(),
-        dueDate: store.globalDueDate,
-        completed: false,
-      };
-      addTask(newTask);
-      setTask("");
-    }
-  };
-
-  return (
-    <div className="flex relative top-12 items-center h-screen  flex-col ">
-      <div className="max-w-115 min-w-80 w-1/2 h-1/2 p-4">
-        <div className="mb-3 flex justify-between items-center">
-          <div className="text-2xl text-black dark:text-white ">Tasks</div>
-          <TaskControllerDropdown/>
+    return (
+        <div className="flex relative top-12 items-center h-screen flex-col ">
+            <div className="max-w-115 min-w-80 w-1/2 h-1/2 p-4">
+                <div className="mb-3 flex justify-between items-center">
+                    <div className="text-2xl text-black dark:text-white ">Tasks</div>
+                    <TaskControllerDropdown/>
+                </div>
+                <AddTaskForm
+                    onSubmit={submitTaskHandler}
+                    task={task}
+                    onChangeTaskName={(e: React.ChangeEvent<HTMLInputElement>) =>
+                        setTask(e.target.value)
+                    }
+                />
+                {Object.keys(tasksGroups).map((groupName) =>
+                        tasksGroups[groupName].length > 0 && (
+                            <TaskList
+                                key={groupName}
+                                groupName={groupName}
+                                tasks={tasksGroups[groupName]}
+                                updateTask={updateTaskHandler(groupName)}
+                            />
+                        )
+                )}
+            </div>
         </div>
-        <AddTaskForm
-          onSubmit={submitTaskHandler}
-          task={task}
-          onChangeTaskName={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setTask(e.target.value)
-          }
-        />
-        {Object.keys(tasksGroups).map((groupName) =>
-            tasksGroups[groupName].length > 0 && (
-              <TaskList
-                key={groupName}
-                groupName={groupName}
-                tasks={tasksGroups[groupName]}
-                updateTask={updateTaskHandler(groupName)}
-              />
-            )
-        )}
-      </div>
-    </div>
-  );
+    );
 }
